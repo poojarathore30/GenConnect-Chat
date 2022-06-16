8e664e2eaf51159a3664fa2c488b5de33efa61d2
Pooja Rathore <poojarathore.nks@gmail.com>
2022-06-16 22:38:44 +0530

    Initial

---
+++ client-user/build/precache-manifest.a4eac9e2dc17adbd10cdcaefa05cc8ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1ebfa28e53229b7e4fb",
+    "url": "/static/css/main.7f38f340.chunk.css"
+  },
+  {
+    "revision": "d1ebfa28e53229b7e4fb",
+    "url": "/static/js/main.e755c0cb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "33b22a69c2d637421939",
+    "url": "/static/js/2.c7b27f9e.chunk.js"
+  },
+  {
+    "revision": "3bdb36c11a882c19981d5d2b44259579",
+    "url": "/static/media/YG.3bdb36c1.PNG"
+  },
+  {
+    "revision": "d1108b7c6fa200b7b903652385efd038",
+    "url": "/index.html"
+  }
+];
